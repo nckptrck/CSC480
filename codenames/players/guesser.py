1224ffd8b84c126d4e64f2a5b6d9fa99263c53d3
robintun <robintun01@gmail.com>
2024-08-29 17:41:49 -0700

    implemented new guesser with dynamic weights

--- codenames/players/guesser.py
+++ codenames/players/guesser.py
@@ -45,13 +45,13 @@ class HumanGuesser(Guesser):
         self.words = words
 
     def get_answer(self):
-        answer_input = input("Guesser makes turn.\nPlease enter a valid Word (or q to move on) >> ")
+        answer_input = input("Guesser makes turn.\nPlease enter a valid Word (or c to move on) >> ")
         type(answer_input)
 
         while not self._is_valid(answer_input):
             print("Input Invalid")
             print(self.words)
-            answer_input = input("Please enter a valid Word (or q to move on) >> ")
+            answer_input = input("Please enter a valid Word (or c to move on) >> ")
             type(answer_input)
         return answer_input
 
